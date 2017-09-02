54ed5398e7a8534736e0700eb4ed11ce5acf6d93
NoThisIsStupider <joshuaeagles1@gmail.com>
2017-09-01 20:23:57 -0400

    Initial Adding Everything
    Extracting Almost Works, but doesn't. Repacking hasn't even been started on.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SA2-AFSTools
 Extractor and Repacker for the Sonic Adventure 2 AFS files. May work on other games, I have no idea.
+
+This comes with luajit for windows, it should work on Linux and stuff but you'll need your own luajit, not actually sure how to run it because I don't have a linux install or VM going, may test it and such later.
+
+To run on windows: 
+(For Powershell, the CMD replacement thing on newer versions of Windows 10)
+Shift+Right Click blank space in this folder
+Select "Open Powershell Window Here" (Alternatively to this first steps, just use cd in any powershell window to the path of luajit, it really doesn't matter, this way is just easier)
+Type: ./luajit main.lua
+Further instructions built into the program
+
+(For CMD)
+Same as Above but no need for a ./ before the command
+
+Also if you're interested the reason the ./ is needed for powershell is because powershell won't run programs in a folder unless you specify you want it from that folder, which can be useful. I don't know why I'm writing this but I found it interesting, and useful.
---
+++ main.lua
@@ -0,0 +1,51 @@
+local struct = require "struct"
+
+contentsOfAFS = {}
+adxForWrite = {}
+
+function readHeader()
+	nomOfADX = struct.unpack("<I", fileAFS:read(4))
+	for i = 1, nomOfADX do
+		local fileADX = {
+			position = struct.unpack("<I", fileAFS:read(4)),
+			length = struct.unpack("<I", fileAFS:read(4))
+		} --end fileADX
+		table.insert(contentsOfAFS, fileADX)
+	end --for
+end--readHeader
+
+function getADX() --get files to package into AFS file
+	
+end
+
+function writeToADX(fileADX, count)
+	fileAFS:seek("set", fileADX.position)
+	ADXForWrite = assert(io.open("workspace/adx/" .. count .. ".adx", "w"))
+	ADXForWrite:write(fileAFS:read(fileADX.length))
+end --writeToADX
+
+function writeToAFS()
+	
+end
+
+repeat
+	print("type \"r\" to read from AFS file titled \"file.afs\" in /workspace/, or \"w\" to write all ADX files in /workspace/adx to a file called output.afs in /workspace/")
+	inputLine = io.read("*line")
+	if inputLine == "r" then
+		fileAFS = io.open("file.afs", "rb") --hardcoded, not gonna make a gui for this
+		fileAFS:read(4) -- set the position to just past the intital 4 bytes which are useless
+		readHeader()
+		for i, fileADX in pairs(contentsOfAFS) do
+			writeToADX(fileADX, i)
+		end --for
+	elseif inputLine == "w" then
+		getADX()
+		for i in pairs(adxForWrite)  do
+			writeToAFS()
+		end --for
+	else
+		print("error, input wasn't \"r\" or \"w\"")
+	end --if elseif
+	print("restart? y/n")
+	inputLine = io.read("*line")
+until inputLine ~= "y"
