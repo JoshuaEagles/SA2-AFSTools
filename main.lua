d7837e94a7f48edb9962cf4c061d968025a3e9ea
NoThisIsStupider <joshuaeagles1@gmail.com>
2017-09-02 20:14:58 -0400

    Insane amount of changes
    Writing is now partially implemented, doesn't work correctly, but is close.

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 local struct = require "struct"
 require "lfs"
 
-adxForWrite = {}
-
-function readHeader()
+function readHeader() --used for reading the afs
 	nomOfADX = struct.unpack("<I", fileAFS:read(4))
 	local contentsOfAFS = {}
 	for i = 1, nomOfADX do
@@ -16,35 +14,67 @@ function readHeader()
 	return contentsOfAFS
 end--readHeader
 
-function getADX() --get files to package into AFS file
-	
-end
-
-function writeToADX(fileADX, count)
+function writeToADX(fileADX, count) -- using for reading the afs
 	fileAFS:seek("set", fileADX.position)
-	ADXForWrite = assert(io.open("workspace/adx/" .. count .. ".adx", "wb"))
+	ADXForWrite = assert(io.open("workspace/adx/" .. numberFormatFourDigits(count) .. ".adx", "wb"))
 	ADXForWrite:write(fileAFS:read(fileADX.length))
 end --writeToADX
 
-function writeToAFS()
-	
-end
+function getADX() --get files to package into AFS file, used for writing
+	local adxForWrite = {}
+	for currentFile in lfs.dir(lfs.currentdir() .. "/workspace/adx") do
+		if string.sub(currentFile, string.len(currentFile) - 3, string.len(currentFile)) == ".adx" then --check file extention
+			local fileADX = {
+				currentFile,
+				lfs.attributes(lfs.currentdir() .. "/workspace/adx/" .. currentFile).size
+			} --end fileADX
+			table.insert(adxForWrite, fileADX) 
+		end --if
+	end --for
+	return adxForWrite
+end --getADX
+
+function writeToAFS(i, fileADX) --used for writing
+	newAFS:seek("set", 8 +(i - 1) * 8) --set it to the next unused portion of the header
+	newAFS:write(struct.pack("<I", offset), struct.pack("<I", fileADX[2]))
+	newAFS:seek("set", offset)
+	local ADXForRead = io.open("workspace/adx/" .. fileADX[1], "rb")
+	newAFS:write(ADXForRead:read(fileADX[2])) 
+	offset = offset + fileADX[2] + 16 --+ 16 is just incase the last file written somehow ends on a multiple of 512
+	offset = offset + offset % 512 --aligns the offset to a multiple of 512, something about byte boundaries or something, idk
+end --write to AFS
+
+-- formats a number to four digits eg. 1 > 0001, 27 > 0027, etc
+function numberFormatFourDigits(number)
+	if number > 999 then
+		return tostring(number)
+	elseif number > 99 then
+		return "0" .. number
+	elseif number > 9 then
+		return "00" .. number
+	else
+		return "000" .. number
+	end --if elseif elseif elseif
+end --numberFormatFourDigits
 
 lfs.mkdir(lfs.currentdir() .. "/workspace")
 lfs.mkdir(lfs.currentdir() .. "/workspace/adx")
 repeat
 	print("type \"r\" to read from AFS file titled \"file.afs\" in /workspace/, or \"w\" to write all ADX files in /workspace/adx to a file called output.afs in /workspace/")
-	inputLine = io.read("*line")
+	local inputLine = io.read("*line")
 	if inputLine == "r" then
 		fileAFS = io.open("workspace/file.afs", "rb") --hardcoded, not gonna make a gui for this
-		fileAFS:read(4) -- set the position to just past the intital 4 bytes which are useless
+		fileAFS:seek("set", 4) -- set the position to just past the intital 4 bytes which are useless
 		for i, fileADX in pairs(readHeader()) do --readHeader returns the length and position of each adx file
 			writeToADX(fileADX, i)
 		end --for
 	elseif inputLine == "w" then
-		getADX()
-		for i in pairs(adxForWrite)  do
-			writeToAFS()
+		local adxForWrite = getADX()
+		offset = 524288
+		newAFS = io.open("workspace/newFile.afs", "wb")
+		newAFS:write("AFS\0" .. struct.pack("<I", #adxForWrite))
+		for i, fileADX in pairs(adxForWrite)  do
+			writeToAFS(i, fileADX)
 		end --for
 	else
 		print("error, input wasn't \"r\" or \"w\"")
